374c9c8fa5bc5375b2df5fdcf2aa0e0572114b9b
NN <95127494+KaningNoppasin@users.noreply.github.com>
2026-01-30 22:18:27 +0700

    add /h1 route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/h1', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
