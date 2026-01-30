ec6162da9b497f8e774f63dd5c505180ce14cdee
NN <95127494+KaningNoppasin@users.noreply.github.com>
2026-01-30 22:02:20 +0700

    add express

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-}
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
