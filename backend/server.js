0db87729e4df4729f564e4a1783077ec449ed0bc
NN <95127494+KaningNoppasin@users.noreply.github.com>
2026-01-30 22:30:56 +0700

    add /json route

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.get('/h1', (req, res) => {
   res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/json', (req, res) => {
+  res.json({ key: 'value' });
 });
 
 app.listen(port, () => {
