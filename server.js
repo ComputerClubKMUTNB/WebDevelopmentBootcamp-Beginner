832b8693b9f01b9920ca19221c6b6372976169a4
NN <95127494+KaningNoppasin@users.noreply.github.com>
2026-02-06 09:07:22 +0700

    change .git path

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
